d797ed541058bcb6c421649a0cf9ab36853016bf
David Amorim <davidfilipeamorim@gmail.com>
2022-05-05 16:13:27 +0100

    Add Form component

--- mobile/src/components/Widget/index.tsx
+++ mobile/src/components/Widget/index.tsx
@@ -1,11 +1,18 @@
 import React, { useRef } from 'react';
 import { TouchableOpacity } from 'react-native';
-import { ChatTeardropDots } from 'phosphor-react-native';
-import BottomSheet from '@gorhom/bottom-sheet';
 import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
+import BottomSheet from '@gorhom/bottom-sheet';
+import { ChatTeardropDots } from 'phosphor-react-native';
+
+import { Options } from '../Options';
+import { Form } from '../Form';
+
+import { feedbackTypes } from '../../utils/feedbackTypes';
 
 import { theme } from '../../theme';
 import { styles } from './styles';
+
+export type FeedbackType = keyof typeof feedbackTypes;
 
 function Widget() {
   const bottomSheetRef = useRef<BottomSheet>(null);
@@ -29,11 +36,13 @@ function Widget() {
 
       <BottomSheet
         ref={bottomSheetRef}
-        snapPoints={[1, 200]}
+        snapPoints={[1, 280]}
         backgroundStyle={styles.modal}
         handleIndicatorStyle={styles.indicator}
       >
-
+        <Form
+          feedbackType="BUG"
+        />
       </BottomSheet>
     </>
   );
